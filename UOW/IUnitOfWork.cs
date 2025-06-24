920610f4b27bb9e126397c45610a173cb282daff
SantyOsama <santyosama2@gmail.com>
2025-06-25 00:44:12 +0300

    Unit Of Work (UOW)

---
+++ UOW/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using RakbnyMa_aak.Repositories.Interfaces;
+
+namespace RakbnyMa_aak.UOW
+{
+    public interface IUnitOfWork
+    {
+        IDriverRepository DriverRepository {  get; }
+        IUserRepository UserRepository { get; }
+        Task<int> CompleteAsync(); // SaveChanges
+        Task RollbackAsync(); // Rollback 
+
+    }
+}
---
+++ UOW/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RakbnyMa_aak.Models;
+using RakbnyMa_aak.Repositories.Implementations;
+using RakbnyMa_aak.Repositories.Interfaces;
+
+namespace RakbnyMa_aak.UOW
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public IUserRepository UserRepository { get; }
+
+        public IDriverRepository DriverRepository { get; }
+
+
+        public UnitOfWork(AppDbContext dBContext, UserManager<ApplicationUser> userManager)
+        {
+            _context = dBContext;
+            DriverRepository = new DriverRepository(_context);
+          //  UserRepository= new UserRepository(userManager);
+
+        }
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task RollbackAsync()
+        {
+            await _context.Database.RollbackTransactionAsync();
+        }
+    }
+}

1d1e921becaa84d7181511595eefdc12aa4645bf
SantyOsama <santyosama2@gmail.com>
2025-07-08 12:59:06 +0300

    Remove port

--- Program.cs
+++ Program.cs
@@ -173,11 +173,6 @@ namespace RakbnyMa_aak
 
             builder.Services.AddAuthorization();
 
-
-            var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-            builder.WebHost.UseUrls($"http://*:{port}");
-
-
             var app = builder.Build();
 
             //Seed the database with initial data

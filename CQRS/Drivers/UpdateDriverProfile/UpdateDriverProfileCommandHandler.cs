4e7d6d456fa0674bccc8307b16ca700e5cee8234
hossam-alashwal <hossamadel5556@gmail.com>
2025-07-09 23:23:34 +0300

    Update driver profile

---
+++ CQRS/Drivers/UpdateDriverProfile/UpdateDriverProfileCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using RakbnyMa_aak.GeneralResponse;
+using RakbnyMa_aak.Models;
+
+namespace RakbnyMa_aak.CQRS.Drivers.UpdateDriverProfile
+{
+    public class UpdateDriverProfileCommandHandler : IRequestHandler<UpdateDriverProfileCommand, Response<string>>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UpdateDriverProfileCommandHandler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Response<string>> Handle(UpdateDriverProfileCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(request.DriverUserId);
+            if (user == null)
+                return Response<string>.Fail("User not found");
+
+            user.FullName = request.Dto.FullName;
+            user.Email = request.Dto.Email;
+            user.PhoneNumber = request.Dto.PhoneNumber;
+
+            var result = await _userManager.UpdateAsync(user);
+            return result.Succeeded
+                ? Response<string>.Success("Profile updated successfully")
+                : Response<string>.Fail("Error: " + string.Join(", ", result.Errors.Select(e => e.Description)));
+        }
+    }
+
+}

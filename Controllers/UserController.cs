e7bbc61f6bce15ee047f19c39eb352c7ada28b24
hossam-alashwal <hossamadel5556@gmail.com>
2025-07-13 15:34:46 +0300

    get user all booking based on booking status

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,11 +4,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RakbnyMa_aak.CQRS.Features.Auth.Queries.GetUserById;
 using RakbnyMa_aak.CQRS.Features.Trips.Queries.GetMyBookedTrips;
+using RakbnyMa_aak.CQRS.Queries.GetUserBookings;
 using RakbnyMa_aak.CQRS.Users.UpdateUserProfile;
 using RakbnyMa_aak.DTOs.UserDTOs;
 using RakbnyMa_aak.DTOs.UserDTOs.UpdateProfileDTOs;
 using RakbnyMa_aak.GeneralResponse;
 using System.Security.Claims;
+using static RakbnyMa_aak.Utilities.Enums;
 namespace RakbnyMa_aak.Controllers
 {
     [Route("api/[controller]")]
@@ -60,6 +62,18 @@ namespace RakbnyMa_aak.Controllers
             var result = await _mediator.Send(command);
 
             return Ok(result);
+        }
+
+
+        [Authorize(Roles = "User")]
+        [HttpGet("user/my-bookings")]
+        public async Task<IActionResult> GetMyBookings([FromQuery] int page = 1, [FromQuery] int pageSize = 10, [FromQuery] RequestStatus? status = null)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var query = new GetMyBookingsQuery(page, pageSize, status, userId);
+            var result = await _mediator.Send(query);
+            return result.IsSucceeded ? Ok(result) : BadRequest(result);
         }
 
 
